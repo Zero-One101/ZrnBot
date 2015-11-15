d7c5f508326ce1ae46ad3b4eb6db8d6b5b3d03e4
Zero-One101 <stephenbadrick@gmail.com>
2015-11-15 18:29:52 +0000

    Created IrcUri wrapper
    Ensures the URI of an IRC network is valid

---
+++ ZrnBot/IO/IrcUri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ZrnBot.IO
+{
+    class IrcUri
+    {
+        public Uri NetworkUri { get; }
+
+        public IrcUri(string server, int port)
+        {
+            try
+            {
+                if (!server.StartsWith("irc://"))
+                {
+                    server = "irc://" + server;
+                }
+
+                NetworkUri = new Uri(server + ":" + port);
+            }
+            catch (UriFormatException e)
+            {
+                throw;
+            }
+        }
+    }
+}
